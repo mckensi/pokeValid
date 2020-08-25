f0f2e08f63a7485a860fcadf04ddae9fed7555ab
DANIELMCKENSI <DANIEL.STEVEN.MURCIA@gmail.com>
2020-08-25 10:52:11 -0500

    Initial Commit

---
+++ PokeDeskValidTests/PokeDeskValidTests.swift
@@ -0,0 +1,34 @@
+//
+//  PokeDeskValidTests.swift
+//  PokeDeskValidTests
+//
+//  Created by Daniel Steven Murcia Almanza on 25/08/20.
+//  Copyright © 2020 selvamatic. All rights reserved.
+//
+
+import XCTest
+@testable import PokeDeskValid
+
+class PokeDeskValidTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
